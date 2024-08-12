0e479c56ca2ef2acc3fbe03e2f784e6f304ea4dd
RoaringInferno <jj2t17@gmail.com>
2024-08-12 09:34:39 -0400

    Formatting
    Cleaned up equation display to remove 1 coefficients, 0 coefficients, and 1 exponents
    
    Solution display now displays exponents when duplicate factors

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ namespace generate
         const mprgen::integer factor_min = -1*factor_max;
 
         mprgen::IntegerGen ig({factor_min, factor_max});
-        
         mprgen::integer factors[factor_count];
         for (uint64_t i = 0; i < factor_count; i++)
         {
@@ -56,19 +55,62 @@ namespace generate
         } while (!carry); // If the carry bit is set left over
 
         // Constructing the problem
+        auto generate_term_string = [](const mprgen::integer& coeff, const uint32_t& order) -> std::string
+        {
+            if (coeff == 0)
+            {
+                return "";
+            }
+            if (coeff == 1)
+            {
+                if (order == 0)
+                {
+                    return "1";
+                }
+                if (order == 1)
+                {
+                    return "x";
+                }
+                return "x^" + std::to_string(order);
+            }
+            std::string coeff_str = std::to_string(std::abs(coeff));
+            if (order == 0)
+            {
+                return coeff_str;
+            }
+            if (order ==1)
+            {
+                return coeff_str + "x";
+            }
+            return coeff_str + "x^" + std::to_string(order);
+        };
+        auto generate_sign_string = [](const mprgen::integer& coeff) -> std::string
+        {
+            return coeff >= 0 ? " + " : " - ";
+        };
+
         std::string solution;
+        std::unordered_map<mprgen::integer, uint32_t> factor_map;
         for (uint64_t i = 0; i < factor_count; i++)
         {
-            std::string factor_str = "(x " + (std::string)(factors[i] > 0 ? "+" : "-") + " " + std::to_string(std::abs(factors[i])) + ")";
-            solution += factor_str;
+            factor_map[factors[i]]++;
         }
-        std::string problem = (coefficients[factor_count] > 0 ? "" : "-") + std::to_string(coefficients[factor_count]) + "x^" + std::to_string(factor_count);
-        for (uint32_t i = factor_count-1; i > 0; i++)
+        for (auto& factor : factor_map)
         {
-            std::string coeff_str = (coefficients[i] > 0 ? " + " : " - ") + std::to_string(coefficients[i]) + "x^" + std::to_string(i);
+            solution += "(x" + generate_sign_string(factor.first) + std::to_string(std::abs(factor.first)) + ")";
+            if (factor.second > 1)
+            {
+                solution += "^" + std::to_string(factor.second);
+            }
+        }
+        std::string problem = generate_term_string(coefficients[factor_count], factor_count);
+        for (uint32_t i = factor_count-1; i > 0; i--)
+        {
+            if (coefficients[i] == 0) { continue; }
+            std::string coeff_str = generate_sign_string(coefficients[i])+ generate_term_string(coefficients[i], i);
             problem += coeff_str;
         }
-        problem += (coefficients[0] > 0 ? " + " : " - ") + std::to_string(coefficients[0]);
+        problem += generate_sign_string(coefficients[0]) + generate_term_string(coefficients[0], 0);
 
         return mprgen::MathProblem(problem, solution);
     };
@@ -79,44 +121,12 @@ int main()
     const uint32_t factor_count = 2;
     const uint32_t problem_count = 50;
     std::vector<mprgen::MathProblem> problems(problem_count);
-    /*
+
     for (uint32_t i = 0; i < problem_count; i++)
     {
         problems[i] = generate::polynomial_factoring(factor_count);
     }
-    */
 
-
-    mprgen::IntegerGen ig({-20, 20});
-    auto generate = [&ig]() -> mprgen::integer
-    {
-        mprgen::integer x = ig.generate();
-        while (x == 0)
-        {
-            x = ig.generate();
-        }
-        return x;
-    };
-
-    auto generatesign = [](mprgen::integer x) -> std::string
-    {
-        return x > 0 ? "+" : "-";
-    };
-    auto printstring = [&generatesign](mprgen::integer x) -> std::string
-    {
-        return std::string(" " +generatesign(x) + " " + std::to_string(std::abs(x)));
-    };
-    for (uint32_t i = 0; i < problem_count; i++)
-    {
-        mprgen::integer x1 = generate();
-        mprgen::integer x2 = generate();
-        mprgen::integer c = x1*x2;
-        mprgen::integer b = x1+x2;
-        problems[i] = mprgen::MathProblem(
-            "x^2" + printstring(b) + "x" + printstring(c),
-            "(x" + printstring(x1) + ")(x" + printstring(x2) + ")"
-        );
-    }
     ask(problems);
     return 0;
 }
