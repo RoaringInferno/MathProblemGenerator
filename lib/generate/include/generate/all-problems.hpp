ec2e49ab92165aa9254e3aae4413600618370f81
RoaringInferno <jj2t17@gmail.com>
2024-08-26 09:34:26 -0500

    Added a copypasta to app-problems.hpp
    Makes implementation of new problems easier

--- lib/generate/include/generate/all-problems.hpp
+++ lib/generate/include/generate/all-problems.hpp
@@ -3,3 +3,38 @@
 #include "polynomial-factoring.hpp"
 #include "dot-product.hpp"
 #include "cross-product.hpp"
+
+/*
+#pragma once
+
+#include "generate.hpp"
+
+namespace generate
+{
+    namespace example
+    {
+        struct parameters
+        {
+            mprgen::integer max;
+            mprgen::integer min;
+        };
+
+        mprgen::MathProblem problem(
+            const parameters& params = {9, -9}
+        );
+
+        void problem_set(
+            const parameters& params,
+            const iterator_range_t& range
+        );
+    };
+}
+
+void generate::example::problem_set(const parameters &params, const iterator_range_t &range)
+{
+    for (generate::problem_iterator_t it = range.begin; it != range.end; it++)
+    {
+        *it = problem(params);
+    }
+}
+*/
--- lib/generate/include/generate/cross-product.hpp
+++ lib/generate/include/generate/cross-product.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#pragma once
-
 #include "generate.hpp"
 
 namespace generate
